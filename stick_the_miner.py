a2a72e763c7cb7b1e74c2943c6bbf180581f4261
Pichaya Puttekulangkura <micky47561@gmail.com>
2021-09-10 12:32:43 +0700

    Added colorlog package for output with colors

--- stick_the_miner.py
+++ stick_the_miner.py
@@ -6,6 +6,10 @@ from Crypto.Hash import keccak
 from eth_abi.packed import encode_abi_packed
 import random
 import time
+import logging, colorlog
+
+SALTCOLORLEVEL = 1
+RESULTCOLORLEVEL = 2
 
 chain_id = 1  # eth main net who mine others chain for god sake // nah JK I think you know how to do chain ID
 entropy = 0x949f147e5fb733f43d887bd3f9455dad768029a75baa8c63ec2d749439935d59  # loot / main net
@@ -18,6 +22,26 @@ diff = 1  # just read from the contract or front end
 
 target = 2 ** 256 / diff
 
+def setup_logger():
+    logging.addLevelName(RESULTCOLORLEVEL, 'RESULT')
+    logging.addLevelName(SALTCOLORLEVEL, 'SALT')
+
+    formatter = colorlog.ColoredFormatter("%(log_color)s%(levelname)-8s%(reset)s %(message_log_color)s%(message)s",
+    log_colors={'RESULT': 'cyan' ,'SALT': 'blue'},
+    secondary_log_colors={
+		'message': {
+			'RESULT':    'cyan',
+			'SALT':    'white,bg_blue',
+		}
+	})
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger = logging.getLogger()
+    logger.addHandler(handler)
+    logger.setLevel('RESULT')
+    logger.setLevel('SALT')
+
+    return logger
 
 def pack_mine(chain_id, entropy, gemAddr, senderAddr, kind, nonce, salt) -> bytes:
     return encode_abi_packed(['uint256', 'uint256', 'address', 'address', 'uint', 'uint', 'uint'],
@@ -30,10 +54,10 @@ def mine(packed) -> (str, int):
     hx = k.hexdigest()
     return hx, int(hx, base=16)
 
-
 def get_salt() -> int:
     return random.randint(1, 2 ** 123)  # can probably go to 256 but 123 probably enough
 
+logger = setup_logger()
 
 i = 0
 st = time.time()
@@ -44,7 +68,9 @@ while True:
     hx, ix = mine(pack_mine(chain_id, entropy, gemAddr, userAddr, kind, nonce, salt))
 
     if ix < target:
-        print("done! here's the salt - ", salt)
+        logger.log(RESULTCOLORLEVEL, "done! here's the salt")
+        logger.log(SALTCOLORLEVEL,str(salt))
         break
+    
     if i % 5000 == 0:
         print(f'iter {i}, {i / (time.time() - st)} avg iter per sec')
